606f85dec1863cab93823f1bed1a3a74e5d553c6
Nikita Nikitin <berozzy@gmail.com>
2012-12-15 16:56:17 +0200

    Solved commit troubles

--- p2lj.php
+++ p2lj.php
@@ -1,41 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-function p2lj ($username, $password, $subject, $text) {
-  error_reporting(0);
-  global $encoding;
-  require 'xmlrpc.inc';
-
-  $encoding = 'UTF-8';
-  $date = time();
-
-  $post = array(
-    'username' => new xmlrpcval($username, 'string'),
-    'password' => new xmlrpcval($password, 'string'),
-    'event' => new xmlrpcval($text, 'string'),
-    'subject' => new xmlrpcval($subject, 'string'),
-    'lineendings' => new xmlrpcval('unix', 'string'),
-    'year' => new xmlrpcval(date('Y', $date), 'int'),
-    'mon' => new xmlrpcval(date('m', $date), 'int'),
-    'day' => new xmlrpcval(date('d', $date), 'int'),
-    'hour' => new xmlrpcval(date('G', $date), 'int'),
-    'min' => new xmlrpcval(date('i', $date), 'int'),
-    'ver' => new xmlrpcval(2, 'int')
-  );
-
-  $message = new xmlrpcmsg('LJ.XMLRPC.postevent', array(
-    new xmlrpcval($post, 'struct')
-  ));
-
-  $client = new xmlrpc_client('/interface/xmlrpc', 'www.livejournal.com', 80);
-  $client->request_charset_encoding = $encoding;
-  $request = $client->send($message);
-
-  if ($request->faultCode()) return false;
-  $answer = php_xmlrpc_decode($request->value());
-  return $answer;
-}
-=======
 function p2lj ($login, $passw, $subj, $text, $props = false) {
   error_reporting(0);
   global $xmlrpc_internalencoding;
@@ -74,4 +38,3 @@ function p2lj ($login, $passw, $subj, $text, $props = false) {
 
   return $result->faultCode() ? false : php_xmlrpc_decode($result->value());
 }
->>>>>>> Added optional props support
