a771d347a95b964bfa306a963d437239a437fa1a
Nikita Nikitin <berozzy@gmail.com>
2012-12-15 14:20:47 +0200

    Version 2.0

--- README.md
+++ README.md
@@ -3,10 +3,25 @@ p2livejournal
 
 LJ via PHP.
 
-To post using this script, use the function inside `/lib/raw.E2toLJ.php`.  
+To post using this script, upload `/lib/` on your server and include `/lib/raw.p2lj.php` and use function `p2lj()`.
+Function needs folowing parameters:
+`login` — your login in LJ system,
+`passw` — password of your account,
+`subj` — subject of the entry,
+`text` — entire text.
+
 It returns an array or **false**.
 
 Array contains:  
 `itemid` – post ID,  
 `url` – post URL,  
 `anum` — authorization token.  
+
+Example of using script:
+
+	@include '/lib/raw.p2lj.php';
+
+	if ($_POST) {
+		$post = p2lj($_POST['login'], $_POST['password'], $_POST['subject'], $_POST['message']);
+  		echo is_array($post) ? $post['url'] : 'Error while posting';
+	}
---
+++ lib/raw.p2lj.php
@@ -0,0 +1,28 @@
+function p2lj ($login, $passw, $subj, $text) {
+  @include "lib/xmlrpc.inc";
+  $xmlrpc_internalencoding = 'UTF-8';
+  $date = time();
+  $post = array(
+    "username" => new xmlrpcval( $login, "string" ),
+    "password" => new xmlrpcval( $passw, "string" ),
+    "event" => new xmlrpcval( $text, "string" ),
+    "subject" => new xmlrpcval( $subj, "string" ),
+    "lineendings" => new xmlrpcval( "unix", "string" ),
+    "year" => new xmlrpcval( date( "Y", $date ), "int" ),
+    "mon" => new xmlrpcval( date( "m", $date ), "int" ),
+    "day" => new xmlrpcval( date( "d", $date ), "int" ),
+    "hour" => new xmlrpcval( date( "G", $date ), "int" ),
+    "min" => new xmlrpcval( date( "i", $date ), "int" ),
+    "ver" => new xmlrpcval( 2, "int" )
+  );
+  $f = @new xmlrpcmsg( 'LJ.XMLRPC.postevent', array(
+    new xmlrpcval( $post, "struct" )
+  ));
+  $c = @new xmlrpc_client( "/interface/xmlrpc", "www.livejournal.com", 80 );
+  @$c->request_charset_encoding = "UTF-8";
+  $r = @$c->send( $f );
+  if ( !$r->faultCode() ) {
+    $v = php_xmlrpc_decode( $r->value() );
+    return $v;
+  } else return false;
+}
