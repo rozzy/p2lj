433d44889d3b6402e2446675b8c2257873b08fd4
Nikita Nikitin <berozzy@gmail.com>
2012-12-15 16:52:44 +0200

    Added optional props support

--- p2lj.php
+++ p2lj.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 function p2lj ($username, $password, $subject, $text) {
   error_reporting(0);
   global $encoding;
@@ -34,3 +35,43 @@ function p2lj ($username, $password, $subject, $text) {
   $answer = php_xmlrpc_decode($request->value());
   return $answer;
 }
+=======
+function p2lj ($login, $passw, $subj, $text, $props = false) {
+  error_reporting(0);
+  global $xmlrpc_internalencoding;
+  require 'xmlrpc.inc';
+
+  $xmlrpc_internalencoding = 'UTF-8';
+  $time = time();
+
+  $_props = array();
+  if ($props) foreach ($props as $key => $value) {
+    $_props[$key] = new xmlrpcval( $value, 'string' );
+  };
+
+  $config = array(
+    'username' => new xmlrpcval( $login, 'string' ),
+    'password' => new xmlrpcval( $passw, 'string' ),
+    'event' => new xmlrpcval( $text, 'string' ),
+    'subject' => new xmlrpcval( $subj, 'string' ),
+    'lineendings' => new xmlrpcval( 'unix', 'string' ),
+    'year' => new xmlrpcval( date( 'Y', $time ), 'int' ),
+    'mon' => new xmlrpcval( date( 'm', $time ), 'int' ),
+    'day' => new xmlrpcval( date( 'd', $time ), 'int' ),
+    'hour' => new xmlrpcval( date( 'G', $time ), 'int' ),
+    'min' => new xmlrpcval( date( 'i', $time ), 'int' ),
+    'ver' => new xmlrpcval( 2, 'int' ),
+    'props' => new xmlrpcval( $_props, 'struct' )
+  );
+
+  $msg = new xmlrpcmsg('LJ.XMLRPC.postevent', array(
+    new xmlrpcval($config, 'struct')
+  ));
+
+  $lj = new xmlrpc_client('/interface/xmlrpc', 'www.livejournal.com', 80);
+  $lj->request_charset_encoding = $xmlrpc_internalencoding;
+  $result = $lj->send($msg);
+
+  return $result->faultCode() ? false : php_xmlrpc_decode($result->value());
+}
+>>>>>>> Added optional props support
