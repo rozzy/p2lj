965e5d58293471989026107008f372ba91e31930
George Timoschenko <malfermu@malfer.mu>
2012-12-15 18:23:05 +0400

    Style & security

--- p2lj.php
+++ p2lj.php
@@ -1,38 +1,36 @@
 <?php
 
-function p2lj ($login, $passw, $subj, $text) {
-  global $xmlrpc_internalencoding;
+function p2lj ($username, $password, $subject, $text) {
   error_reporting(0);
-  require "xmlrpc.inc";
+  global $encoding;
+  require 'xmlrpc.inc';
 
-  $xmlrpc_internalencoding = 'UTF-8';
+  $encoding = 'UTF-8';
   $date = time();
 
   $post = array(
-    "username" => new xmlrpcval( $login, "string" ),
-    "password" => new xmlrpcval( $passw, "string" ),
-    "event" => new xmlrpcval( $text, "string" ),
-    "subject" => new xmlrpcval( $subj, "string" ),
-    "lineendings" => new xmlrpcval( "unix", "string" ),
-    "year" => new xmlrpcval( date( "Y", $date ), "int" ),
-    "mon" => new xmlrpcval( date( "m", $date ), "int" ),
-    "day" => new xmlrpcval( date( "d", $date ), "int" ),
-    "hour" => new xmlrpcval( date( "G", $date ), "int" ),
-    "min" => new xmlrpcval( date( "i", $date ), "int" ),
-    "ver" => new xmlrpcval( 2, "int" )
+    'username' => new xmlrpcval($username, 'string'),
+    'password' => new xmlrpcval($password, 'string'),
+    'event' => new xmlrpcval($text, 'string'),
+    'subject' => new xmlrpcval($subject, 'string'),
+    'lineendings' => new xmlrpcval('unix', 'string'),
+    'year' => new xmlrpcval(date('Y', $date), 'int'),
+    'mon' => new xmlrpcval(date('m', $date), 'int'),
+    'day' => new xmlrpcval(date('d', $date), 'int'),
+    'hour' => new xmlrpcval(date('G', $date), 'int'),
+    'min' => new xmlrpcval(date('i', $date), 'int'),
+    'ver' => new xmlrpcval(2, 'int')
   );
 
   $message = new xmlrpcmsg('LJ.XMLRPC.postevent', array(
-    new xmlrpcval($post, "struct")
+    new xmlrpcval($post, 'struct')
   ));
 
   $client = new xmlrpc_client('/interface/xmlrpc', 'www.livejournal.com', 80);
-  $client->request_charset_encoding = $xmlrpc_internalencoding;
+  $client->request_charset_encoding = $encoding;
   $request = $client->send($message);
 
-  if (!$request->faultCode()) {
-    $answer = php_xmlrpc_decode($request->value());
-    return $answer;
-  } else return false;
-
+  if ($request->faultCode()) return false;
+  $answer = php_xmlrpc_decode($request->value());
+  return $answer;
 }
